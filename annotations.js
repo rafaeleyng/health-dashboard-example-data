b9b38fbb717814c283d508644324126b9ebf647e
rafaeleyng <rafaeleyng@gmail.com>
2020-10-03 16:26:17 -0300

    simplify data by removing some samples

--- annotations.js
+++ annotations.js
@@ -9,13 +9,13 @@ const tagAnnotations = (annotations, tag) => {
 }
 
 const surgeries = [
-  { time: '2018-06-01', title: 'Surgery A' },
-  { time: '2020-02-10', title: 'Surgery B' },
+  { time: '2020-01-01', title: 'Surgery A' },
+  { time: '2020-03-10', title: 'Surgery B' },
 ]
 
 const medicines = [
-  { time: '2018-08-01', timeEnd: '2018-09-01', title: 'Medicine X', text: 'Prescribed by doctor Anna' },
-  { time: '2019-10-10', timeEnd: '2020-03-20', title: 'Medicine Y', text: 'Taking 10mg a day' },
+  { time: '2019-08-01', timeEnd: '2019-09-01', title: 'Medicine X', text: 'Prescribed by doctor Anna' },
+  { time: '2020-02-10', timeEnd: '2020-03-20', title: 'Medicine Y', text: 'Taking 10mg a day' },
 ]
 
 const annotations = [
